275e45ec7bbc2934084ba01b3eb16f6d1b77828a
Runjie Zhao <ruz222@lehigh.edu>
2020-09-04 16:15:01 -0400

    py

--- industry.py
+++ industry.py
@@ -1,9 +1,27 @@
+from tkinter import * 
 import tkinter
 
+#工业
 class Industry:
-    a=1
+    Nalgfar = 'nalgfar'
+
+#材料成本
+def MaterialCost():
+    return
+    #win_cost
+
+def Ship_Choice():
+    ship_win=tkinter.Tk()
+    ship_win.title("战舰选择")
+    ship_win.geometry('800x500')
+    win.mainloop()
+
 
 win=tkinter.Tk()
 win.title("工业")
 win.geometry('1000x500')
+button1=Button(win,text='成本',command=Ship_Choice) 
+
+button1.pack()
+button1.place(x=10,y=10)
 win.mainloop()
